e33d1617d686b96f337bd108dfcfbfc2fe1840ec
giovanna <serejo3532003@gmail.com>
2023-11-02 12:12:41 -0400

    primeiras aulas JavaScript

---
+++ Aula11/ex008.js
@@ -0,0 +1,17 @@
+
+/*
+var vel = 12
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if(vel >60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
+*/
+
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é estrangeiro!')
+}
